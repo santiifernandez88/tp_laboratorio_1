d71d10b1984f8b0d06a2615a88b3e37fda3709ac
santiifernandez88 <98592413+santiifernandez88@users.noreply.github.com>
2022-06-11 23:45:36 -0300

    Delete carpeta para tp3/tp3 directory

--- carpeta para tp3/tp3/parser.h
+++
@@ -1,3 +0,0 @@
-int parser_PassengerFromText(FILE* pFile , LinkedList* pArrayListPassenger);
-int parser_PassengerFromBinary(FILE* pFile , LinkedList* pArrayListPassenger);
-int parser_IdFromText(FILE* pFile , LinkedList* pArrayListPassenger, int* id);
